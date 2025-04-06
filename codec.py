1d305ee4b592c54c8e91a6b352f3ddeed9ea4b6e
NicolasAdler <nca44@miami.edu>
2025-04-06 19:53:48 -0400

    Add dbFs to dBSPL conversion function to codec.py

--- codec.py
+++ codec.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 from scipy.signal import get_window
 from utils import *
+import numpy as np
 
 
 def fft_analysis(audio, frame_size, hop_size, window_type='hann'):
@@ -22,9 +23,22 @@ def identify_maskers(spl_spectrum, sample_rate, frame_size):
 
     return tonal_maskers, noise_maskers
 
+def convertTo_dbFS(audio, spl, frame_size, hop_size, window_type):
+    """
+    Converts per-frame RMS to dBFS and zeroes SPL where dBFS is ≤ -96 dB.
+    """
+    frames = frame_audio(audio, frame_size, hop_size)
+    window = get_window(window_type, frame_size)
+    windowed = frames * window
+
+    rms_vals = np.sqrt(np.mean(windowed**2, axis=1)) 
+    dbfs = 20 * np.log10(rms_vals) 
+    spl[dbfs <= -96] = 0
+
+    return dbfs, spl
 
 if __name__ == '__main__':
-    filename = 'audio/queen.wav'
+    filename = '/Users/nicolasadler/mue610/mp3_codec/mp3_codec/flute.wav'
     frame_size = 1024
     hop_size = frame_size // 2
     window_type = 'hann'
@@ -37,18 +51,19 @@ if __name__ == '__main__':
     frame_idx = 10  # arbitrary frame idx
     freqs = np.fft.rfftfreq(frame_size, d=1 / fs)
 
-    # Plot SPL normalized FFT
     plt.figure(figsize=(8, 4))
-    plt.plot(freqs, spl[frame_idx])
-    plt.title(f'FFT - Frame {frame_idx} (Post SPL Normalization)')
+    plt.plot(freqs, spl[frame_idx], label='Original SPL')
+    convertTo_dbFS(audio, spl, frame_size, hop_size, window_type)
+    plt.plot(freqs, spl[frame_idx], label='After -96 dBFS = 0 dB SPL')
+
+    plt.title(f'FFT - Frame {frame_idx}')
     plt.xlim([0, fs / 2])
     plt.xlabel('Frequency (Hz)')
     plt.ylabel('SPL (dB)')
     plt.grid(True)
+    plt.legend()
     plt.tight_layout()
     plt.show()
 
     # Task 2.2
-    tonal_maskers, noise_maskers = identify_maskers(spl, fs, frame_size)
-
-
+   #tonal_maskers, noise_maskers = identify_maskers(spl, fs, frame_size)
